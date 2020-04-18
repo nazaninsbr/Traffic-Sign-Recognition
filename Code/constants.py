151e718e5d24f0acd629f485cefcfc44d8c2f51a
mahsa <mahsa.ghazvini@gmail.com>
2020-04-18 12:59:28 +0430

    load learning params from default_params in constants

--- Code/constants.py
+++ Code/constants.py
@@ -8,3 +8,13 @@ img_dim = 30
 number_of_classes = 43
 
 generated_files_path = '../Generated_Files/'
+
+class DefaultObj: pass
+default_params = DefaultObj()
+
+default_params.batch_size = 128
+default_params.learning_rate = 0.01
+default_params.optimizer = "SGD"
+default_params.activation_function = "relu"
+default_params.train_valid_split = 0.2
+default_params.loss_function = "categorical_crossentropy"
--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
 from data_reader import read_image_files
-from constants import data_folder_path, train_csv_file_name, test_csv_file_name
+from constants import data_folder_path, train_csv_file_name, test_csv_file_name, default_params
 from model import Model
 from visualizer import draw_multiple_line_plots
 
@@ -10,78 +10,91 @@ def read_data():
 
 def create_and_test_basic_model(X_train, y_train, X_test, y_test):
     print('> Basic model')
-    learning_rate = 0.01
-    optimizer = 'SGD'
+    learning_rate = default_params.learning_rate
+    optimizer = default_params.optimizer
+    activation_function = default_params.activation_function
     epochs = 20
-    batch_size = 32
+    batch_size = default_params.batch_size
     file_save_name = 'basic_model_lr_{}_optimizer_{}_epochs_{}_batch_{}'.format(learning_rate, optimizer, epochs, batch_size)
-    this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer, train_valid_split=0.2, activation_function='relu', epochs=epochs, batch_size=batch_size, loss_function='categorical_crossentropy', use_drop_out=False, file_save_name=file_save_name)
+    this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer,
+                        train_valid_split=default_params.train_valid_split, activation_function=activation_function,
+                        epochs=epochs, batch_size=batch_size, loss_function=default_params.loss_function,
+                        use_drop_out=False, file_save_name=file_save_name)
     this_model.train()
 
 def test_different_activation_functions(X_train, y_train, X_test, y_test):
     print('> Testing activation functions')
-    learning_rate = 0.01
-    optimizer = 'SGD'
+    learning_rate = default_params.learning_rate
+    optimizer = default_params.optimizer
     epochs = 15
-    batch_size = 32
+    batch_size = default_params.batch_size
     history_of_models = {}
     for activation_function in ['relu', 'sigmoid', 'tanh']:
         file_save_name = 'testing_activation_{}_epochs_{}'.format(activation_function, epochs)
-        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer, train_valid_split=0.2, activation_function=activation_function, epochs=epochs, batch_size=batch_size, loss_function='categorical_crossentropy', use_drop_out=False, file_save_name=file_save_name)
+        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer,
+                            train_valid_split=default_params.train_valid_split, activation_function=activation_function,
+                            epochs=epochs, batch_size=batch_size, loss_function=default_params.loss_function,
+                            use_drop_out=False, file_save_name=file_save_name)
         hist = this_model.train()
         history_of_models[activation_function] = hist
-    draw_multiple_line_plots(history_of_models, 
-        field_to_draw='val_acc', 
-        title = 'accuracy on validation data', 
-        y_label = 'accuracy', 
+    draw_multiple_line_plots(history_of_models,
+        field_to_draw='val_acc',
+        title = 'accuracy on validation data',
+        y_label = 'accuracy',
         x_label = 'epoch',
         file_save_name = 'acc_of_different_activation_functions_{}_epochs.png'.format(epochs))
 
 def test_different_optimizers(X_train, y_train, X_test, y_test):
     print('> Testing optimizers')
-    learning_rate = 0.01
-    activation_function = 'relu'
+    learning_rate = default_params.learning_rate
+    activation_function = default_params.activation_function
     epochs = 15
-    batch_size = 32
+    batch_size = default_params.batch_size
     history_of_models = {}
     for optimizer in ['SGD', 'Adam']:
         file_save_name = 'testing_optimizer_{}_epochs_{}'.format(optimizer, epochs)
-        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer, train_valid_split=0.2, activation_function=activation_function, epochs=epochs, batch_size=batch_size, loss_function='categorical_crossentropy', use_drop_out=False, file_save_name=file_save_name)
+        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer,
+                            train_valid_split=default_params.train_valid_split, activation_function=activation_function,
+                            epochs=epochs, batch_size=batch_size, loss_function=default_params.loss_function,
+                            use_drop_out=False, file_save_name=file_save_name)
         hist = this_model.train()
         history_of_models[optimizer] = hist
 
-    draw_multiple_line_plots(history_of_models, 
-        field_to_draw='val_acc', 
-        title = 'accuracy on validation data', 
-        y_label = 'accuracy', 
+    draw_multiple_line_plots(history_of_models,
+        field_to_draw='val_acc',
+        title = 'accuracy on validation data',
+        y_label = 'accuracy',
         x_label = 'epoch',
         file_save_name = 'acc_of_different_optimizers_{}_epochs.png'.format(epochs))
 
 def test_having_dropout(X_train, y_train, X_test, y_test):
     print('> Testing dropout')
-    learning_rate = 0.01
-    activation_function = 'relu'
+    learning_rate = default_params.learning_rate
+    activation_function = default_params.activation_function
     epochs = 100
-    batch_size = 32
-    optimizer = 'SGD'
+    batch_size = default_params.batch_size
+    optimizer = default_params.optimizer
     history_of_models = {}
     for use_drop_out in [True, False]:
         file_save_name = 'testing_dropout_{}_epochs_{}'.format(str(use_drop_out), epochs)
-        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer, train_valid_split=0.2, activation_function=activation_function, epochs=epochs, batch_size=batch_size, loss_function='categorical_crossentropy', use_drop_out=use_drop_out, file_save_name=file_save_name)
+        this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer,
+                            train_valid_split=default_params.train_valid_split, activation_function=activation_function,
+                            epochs=epochs, batch_size=batch_size, loss_function=default_params.loss_function,
+                            use_drop_out=use_drop_out, file_save_name=file_save_name)
         hist = this_model.train()
         history_of_models['Dropout = '+str(use_drop_out)] = hist
 
-    draw_multiple_line_plots(history_of_models, 
-        field_to_draw='val_acc', 
-        title = 'accuracy on validation data', 
-        y_label = 'accuracy', 
+    draw_multiple_line_plots(history_of_models,
+        field_to_draw='val_acc',
+        title = 'accuracy on validation data',
+        y_label = 'accuracy',
         x_label = 'epoch',
         file_save_name = 'acc_of_validation_having_and_not_having_dropout_{}_epochs.png'.format(epochs))
 
-    draw_multiple_line_plots(history_of_models, 
-        field_to_draw='acc', 
-        title = 'accuracy on train data', 
-        y_label = 'accuracy', 
+    draw_multiple_line_plots(history_of_models,
+        field_to_draw='acc',
+        title = 'accuracy on train data',
+        y_label = 'accuracy',
         x_label = 'epoch',
         file_save_name = 'acc_of_train_having_and_not_having_dropout_{}_epochs.png'.format(epochs))
 
@@ -93,12 +106,16 @@ def train_model(X_train, y_train, X_test, y_test):
 
 def load_model_and_get_accuracy(X_train, y_train, X_test, y_test):
     print('> Basic model')
-    learning_rate = 0.01
-    optimizer = 'SGD'
+    learning_rate = default_params.learning_rate
+    activation_function = default_params.activation_function
     epochs = 10
-    batch_size = 32
+    batch_size = default_params.batch_size
+    optimizer = default_params.optimizer
     file_save_name = 'basic_model_lr_{}_optimizer_{}_epochs_{}_batch_{}'.format(learning_rate, optimizer, epochs, batch_size)
-    this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer, train_valid_split=0.2, activation_function='relu', epochs=epochs, batch_size=batch_size, loss_function='categorical_crossentropy', use_drop_out=False, file_save_name=file_save_name)
+    this_model = Model(X_train, y_train, X_test, y_test, learning_rate=learning_rate, optimizer=optimizer,
+                    train_valid_split=default_params.train_valid_split, activation_function=activation_function,
+                    epochs=epochs, batch_size=batch_size, loss_function=default_params.loss_function,
+                    use_drop_out=False, file_save_name=file_save_name)
     this_model.load_trained_model()
 
 def main():
